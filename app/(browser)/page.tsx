5ef6c72e409e2a508a29d8bf3949e3a8bfaa7066
oskarm2003 <oskarm0503@gmail.com>
2023-09-30 15:47:11 +0200

    add global css variables

---
+++ app/(browser)/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./browser.module.css"
+export default function Browser() {
+
+
+    return <div className={styles.wrapper}>
+        <div className={styles.filters}>
+
+        </div>
+    </div>
+
+}
