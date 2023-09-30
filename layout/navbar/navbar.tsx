f0809b358b82b499b6de36812ce053fcf9f89105
Pshemak <przemek1441@o2.pl>
2023-09-30 18:46:23 +0200

    navbar added logo added

---
+++ layout/navbar/navbar.tsx
@@ -0,0 +1,18 @@
+import styles from "./style.module.css"
+import Image from 'next/image'
+
+export default function Navbar() {
+  return (
+    <nav className={styles.header}>
+          <Image
+      src="/assets/logo2.png"
+      width={200}
+      height={200}
+      alt="logo"
+    />
+      <a href="/profile"className={styles.navText}>Profil</a>
+      <text className={styles.navText}>PL/EN</text>
+    </nav>
+  )
+}
+
---
+++ layout/universityCard/universityCard.tsx
@@ -0,0 +1,8 @@
+import styles from "./style.module.css"
+
+export default function UniveristyCard() {
+  return (
+    <></>
+  )
+}
+
