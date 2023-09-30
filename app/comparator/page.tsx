7b763a9376c11e7acc08d4b594f09b85169967e8
Pshemak <przemek1441@o2.pl>
2023-09-30 21:06:00 +0200

    added exemple images

--- app/comparator/page.tsx
+++ app/comparator/page.tsx
@@ -1,9 +1,14 @@
 
 import Navbar from "@/layout/navbar/navbar"
+import UniveristyCard from "@/layout/universityCard/universityCard"
+
 export default function Comparator() {
   return (
     <>
     <Navbar/>
+    <div>
+      <UniveristyCard></UniveristyCard>
+    </div>
     </>
   )
 }
--- layout/universityCard/universityCard.tsx
+++ layout/universityCard/universityCard.tsx
@@ -2,7 +2,12 @@ import styles from "./style.module.css"
 
 export default function UniveristyCard() {
   return (
-    <></>
+    <div className={styles.card}>
+      <div className={styles.cardFade}>
+
+      </div>
+      
+    </div>
   )
 }
 
