b4eedf43a3d7e66cfc7cf32cf58ad9024b962d8b
Wiktor Orda <wiktor.orda@gmail.com>
2023-09-30 22:13:33 +0200

    Register logic implemented on frontend and backend

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,12 +1,16 @@
 "use client";
-import React, { useState } from "react";
-// import { Checkbox, Spacer, Button, Link } from "@nextui-org/react";
+import React, { use, useState } from "react";
 import { Input } from "@nextui-org/input";
 import { Card, CardBody, CardFooter, CardHeader } from "@nextui-org/card";
 import { Divider } from "@nextui-org/divider";
 import { EyeFilledIcon } from "./EyeFilledIcon.js";
 import { EyeSlashFilledIcon } from "./EyeSlashFilledIcon.js";
 import { Button } from "@nextui-org/button";
+import { useUserContext } from "@/contexts/UserContext";
+import { appConstants } from "@/constants/app";
+import { httpresponseType } from "@/types/api";
+import { userClientType } from "@/types/dbModels";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
   const [isVisible, setIsVisible] = useState(false);
@@ -19,6 +23,41 @@ export default function Login() {
     "warning",
     "danger",
   ];
+  const router = useRouter();
+
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+    name: "",
+  });
+  const [userName, setUserName] = useState("");
+
+  const sendRegisterToBackend = async (email: string, password: string) => {
+    console.log("rejestracja");
+    try {
+      const res = await fetch(`${appConstants.appIP}/api/user/register`, {
+        method: "POST",
+        body: JSON.stringify({ email, password, userName }),
+      });
+
+      const resData: httpresponseType<userClientType & { token: string }> =
+        await res.json();
+      if (resData.status === "failed") {
+        const error = resData.error;
+        return;
+      }
+      const { name, id } = resData.data;
+      console.log(resData.data);
+    } catch (err) {
+      console.log(err);
+      return;
+    }
+  };
+  const registerUser = async () => {
+    console.log("register user");
+    sendRegisterToBackend(userData.email, userData.password);
+    router.push("/login");
+  };
 
   return (
     <div className="grid h-screen place-items-center">
@@ -30,6 +69,9 @@ export default function Login() {
             label="Name"
             placeholder="Enter your name"
             color="primary"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setUserName(e.target.value)
+            }
           />
           <Input
             className="mb-2"
@@ -37,11 +79,17 @@ export default function Login() {
             label="Email"
             placeholder="Enter your email"
             color="primary"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setUserData((p) => ({ ...p, email: e.target.value }))
+            }
           />
           <Input
             label="Password"
             variant="bordered"
             placeholder="Enter your password"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setUserData((p) => ({ ...p, password: e.target.value }))
+            }
             endContent={
               <button
                 className="focus:outline-none"
@@ -59,7 +107,9 @@ export default function Login() {
             className="max-w-xs mb-2"
           />
           <Divider className="mb-2" />
-          <Button color="primary">Register</Button>
+          <Button onClick={registerUser} color="primary">
+            Register
+          </Button>
         </CardBody>
       </Card>
     </div>
