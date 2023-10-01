aa037b05a7cbf0205845a333f809460cf5837f7c
oskarm2003 <oskarm0503@gmail.com>
2023-10-01 09:57:53 +0200

    csscsscss

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Checkbox, Spacer, Button, Link } from "@nextui-org/react";
 
 import { Input } from "@nextui-org/input";
@@ -8,14 +8,23 @@ import { Divider } from "@nextui-org/divider";
 import { EyeFilledIcon } from "./EyeFilledIcon.js";
 import { EyeSlashFilledIcon } from "./EyeSlashFilledIcon.js";
 import { useUserContext } from "@/contexts/UserContext";
+import { useRouter } from "next/navigation.js";
 
 export default function Login() {
   const [isVisible, setIsVisible] = useState(false);
   const [userData, setUserData] = useState({
     email: "",
-    password: "",
-  });
-  const { dispatch } = useUserContext();
+    password: ""
+  })
+  const { dispatch, user, loading } = useUserContext()
+  const router = useRouter()
+
+  //await for user change
+  useEffect(() => {
+    if (user.name === '') return
+    router.push('./matches')
+  }, [user])
+
   const toggleVisibility = () => setIsVisible(!isVisible);
   const colors = [
     "default",
@@ -26,31 +35,28 @@ export default function Login() {
     "danger",
   ];
 
+
   return (
-    <div className="grid h-screen place-items-center">
+    <div className={"grid h-screen place-items-center cursor-wait" + (loading ? ' cursor-wait' : '')}>
       <Card>
         <CardHeader>Sign In</CardHeader>
         <CardBody>
           <Input
-            className="mb-4"
+            className={"mb-2" + (loading ? ' cursor-wait' : '')}
             type="email"
             label="Email"
             placeholder="Enter your email"
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setUserData((p) => ({ ...p, email: e.target.value }))
-            }
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUserData(p => ({ ...p, email: e.target.value }))}
             color="primary"
           />
           <Input
             label="Password"
             variant="bordered"
             placeholder="Enter your password"
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setUserData((p) => ({ ...p, password: e.target.value }))
-            }
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUserData(p => ({ ...p, password: e.target.value }))}
             endContent={
               <button
-                className="focus:outline-none"
+                className={"focus:outline-none" + (loading ? ' cursor-wait' : '')}
                 type="button"
                 onClick={toggleVisibility}
               >
@@ -62,16 +68,17 @@ export default function Login() {
               </button>
             }
             type={isVisible ? "text" : "password"}
-            className="max-w-xs mb-4"
+
+            className={"max-w-xs" + (loading ? ' cursor-wait' : '')}
           />
-          <Button
-            onClick={() => dispatch.login(userData.email, userData.password)}
-          >
+          <Button onClick={() => dispatch.login(userData.email, userData.password)} className={(loading ? ' cursor-wait' : '')}>
             Login
           </Button>
         </CardBody>
         <Divider />
       </Card>
+      <button className="hover:text-gray-500 text-gray-900" onClick={() => router.push('./register')}>register first</button>
+
     </div>
   );
 }
