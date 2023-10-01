b0fc5b3addbdd2e05f127c740fb9b5b692d588aa
Dominik Małek <4-tel@protonmail.com>
2023-10-01 09:57:05 +0200

    hghhggh

--- app/swipe/page.tsx
+++ app/swipe/page.tsx
@@ -27,58 +27,58 @@ export default function SwipePage() {
   const { x } = useMousePosition()
 
   if (error === "User not authenticated") return redirect("/")
-  
+
 
   function acceptStudyPath(mouseXPos: number | null, dragAnchor: number | null) {
     if (!mouseXPos || !dragAnchor) return
-    
+
     // getting next study path
     dispatch.getNext()
     setIsDragged(false)
     setDragPoint(null)
-      
+
   }
 
   return (
     <div className='w-full h-full bg-green-300 flex items-center justify-center'>
-        <div className='h-[95%] w-[600px] bg-gray-200 flex items-center just-center p-0 pl-5 pr-5 rounded-md'>
-            <motion.div
-            className={`w-full h-[95%] bg-white relative z-10 rounded-md ${isDragged ? "opacity-80" : ""}`}
-            drag
-            dragConstraints={{
-                top: 0,
-                bottom: 0,
-                left: 10,
-                right: 10
-            }}
-            onDragStart={() => {
-              setIsDragged(true)
-              setDragPoint(x)
-            }}
-         
-            onDragEnd={() => {
-              acceptStudyPath(x, dragPoint)
-            }}
-          
-            >
-              {/* @ts-ignore */}
-                <StudyPathCard loading={loading} error={error} data={data[0]}/>
-            </motion.div>
-        </div>
+      <div className='h-[95%] w-[600px] bg-gray-200 flex items-center just-center p-0 pl-5 pr-5 rounded-md'>
+        <motion.div
+          className={`w-full h-[95%] bg-white relative z-10 rounded-md ${isDragged ? "opacity-80" : ""}`}
+          drag
+          dragConstraints={{
+            top: 0,
+            bottom: 0,
+            left: 10,
+            right: 10
+          }}
+          onDragStart={() => {
+            setIsDragged(true)
+            setDragPoint(x)
+          }}
 
-        { isDragged && !error ? <>
+          onDragEnd={() => {
+            acceptStudyPath(x, dragPoint)
+          }}
+
+        >
           {/* @ts-ignore */}
-          <div className={`fixed w-[50%] h-screen bg-red-200 ${dragPoint > x ? "opacity-80" : "opacity-30"} left-0 border flex justify-center items-center font-bold text-[50px] z-20 transition-opacity`}>
+          <StudyPathCard loading={loading} error={error} data={data[0]} />
+        </motion.div>
+      </div>
+
+      {isDragged && !error ? <>
+        {/* @ts-ignore */}
+        <div className={`fixed w-[50%] h-screen bg-red-200 ${dragPoint > x ? "opacity-80" : "opacity-30"} left-0 border flex justify-center items-center font-bold text-[50px] z-20 transition-opacity`}>
           {/* @ts-ignore */}
-            {dragPoint > x ? <p className='opacity-100'>I don't like it 👎</p> : null}
-          </div>
+          {dragPoint > x ? <p className='opacity-100'>I dont like it 👎</p> : null}
+        </div>
+        {/* @ts-ignore */}
+        <div className={`fixed w-[50%] h-screen bg-green-200 ${dragPoint < x ? "opacity-80" : "opacity-30"} right-0 flex justify-center items-center font-bold text-[50px] z-20 transition-opacity`}>
           {/* @ts-ignore */}
-          <div className={`fixed w-[50%] h-screen bg-green-200 ${dragPoint < x ? "opacity-80" : "opacity-30"} right-0 flex justify-center items-center font-bold text-[50px] z-20 transition-opacity`}>
-          {/* @ts-ignore */}
-            {dragPoint < x ? <p className='blur-0'>I like it 👍</p> : null}
-          </div>
-          <div></div>
-        </> : null}
+          {dragPoint < x ? <p className='blur-0'>I like it 👍</p> : null}
+        </div>
+        <div></div>
+      </> : null}
     </div>
   )
 }
